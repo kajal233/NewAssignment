8a16d0065fd749cd9c964b65220b84de69d19163
kajal233 <89968244+kajal233@users.noreply.github.com>
2021-09-23 19:28:43 +0530

    Add files via upload

---
+++ Kajal/CoreJava/DataType/DataTypesDemo.java
@@ -0,0 +1,37 @@
+package com.datatypes;
+
+public class DataTypesDemo {
+
+	public static void main(String args[]) {
+
+		int length = 6; // local variable for this method
+
+		System.out.println("integer variable " + 6);
+
+		double d = 12.5;
+
+		System.out.println("double variable " + d);
+
+		String dummyvariable = "ram";
+
+		System.out.println("String  variable " + dummyvariable);
+
+		float f = (float) 15.5; // type casting
+
+		System.out.println("float type  variable " + f);
+
+		char c = 'a';
+
+		System.out.println("char  type  variable " + c);
+
+		long l = 1254;
+
+		System.out.println("long type  variable " + l);
+
+		byte b = 3;
+
+		System.out.println("long type  variable " + b);
+
+	}
+
+}
